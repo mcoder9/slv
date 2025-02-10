6604e635ca784fcce0ad8c73f87e20f57e990ad0
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2025-02-10 11:51:14 +0100

    fix - uploadExe

--- cli/uploadExe.ts
+++ cli/uploadExe.ts
@@ -10,7 +10,7 @@ const uploadExe = async () => {
   console.log(`Uploading slv v${version} to Solana`)
   await Promise.all(compileTargets.map(async (target) => {
     const cmd =
-      `aws --endpoint-url=https://278a7109e511280594fe6a2ebb778333.r2.cloudflarestorage.com/slv s3 cp ${target}-exe.tar.gz s3://slv/${version}/${target}-exe.tar.gz --content-disposition 'attachment; filename=install'`
+      `aws --endpoint-url=https://278a7109e511280594fe6a2ebb778333.r2.cloudflarestorage.com/slv s3 cp slv-${target}-exe.tar.gz s3://slv/${version}/${target}-exe.tar.gz --content-disposition 'attachment; filename=install'`
     const result = await spawnSync(cmd, './dist')
     console.log(result.message)
   }))
