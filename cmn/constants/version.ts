bbb23efd4f53457a6e0ad6415010daf09f51ed0b
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2025-02-26 14:14:34 +0100

    add ci/cd

--- cmn/constants/version.ts
+++ cmn/constants/version.ts
@@ -6,8 +6,8 @@ export const VERSION = '0.6.0'
 
 // Component versions
 export const VERSION_SOLANA_TESTNET = '2.1.13'
-export const VERSION_SOLANA_MAINNET = '2.1.1'
-export const VERSION_JITO_TESTNET = '2.1.13'
-export const VERSION_JITO_MAINNET = '2.1.1'
-export const VERSION_FIREDANCER_TESTNET = '0.305.20111'
+export const VERSION_SOLANA_MAINNET = '2.1.14'
+export const VERSION_JITO_TESTNET = '2.1.14'
+export const VERSION_JITO_MAINNET = '2.1.14'
+export const VERSION_FIREDANCER_TESTNET = '0.402.20113'
 export const VERSION_FIREDANCER_MAINNET = '0.305.20111'
