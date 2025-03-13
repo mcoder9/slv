d8e9984705333dd918ab820a90969ce7af4f3a8a
KishiTheMechanic <s.kishi@elsoul.nl>
2025-03-13 19:37:27 +0100

    fix eslint & prettier for the backend including tsx

--- cf-workers-template/contact-email/src/email/utils/Fonts.tsx
+++ cf-workers-template/contact-email/src/email/utils/Fonts.tsx
@@ -4,30 +4,34 @@ type Props = {
   lang: 'ja' | 'en'
 }
 
-export default function Fonts({lang}:Props) {
+export default function Fonts({ lang }: Props) {
   return (
     <>
-      {lang === 'en' && (<Font
-        fontFamily="Inter"
-        fallbackFontFamily="sans-serif"
-        webFont={{
-          url: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700&display=swap',
-          format: 'woff2'
-        }}
-        fontWeight={300}
-        fontStyle="normal"
-      />)}
-      
-      {lang === 'ja' && (<Font
-        fontFamily="Noto Sans JP"
-        fallbackFontFamily="sans-serif"
-        webFont={{
-          url: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;500;700&display=swap',
-          format: 'woff2'
-        }}
-        fontWeight={300}
-        fontStyle="normal"
-      />)}
+      {lang === 'en' && (
+        <Font
+          fontFamily="Inter"
+          fallbackFontFamily="sans-serif"
+          webFont={{
+            url: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700&display=swap',
+            format: 'woff2'
+          }}
+          fontWeight={300}
+          fontStyle="normal"
+        />
+      )}
+
+      {lang === 'ja' && (
+        <Font
+          fontFamily="Noto Sans JP"
+          fallbackFontFamily="sans-serif"
+          webFont={{
+            url: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;500;700&display=swap',
+            format: 'woff2'
+          }}
+          fontWeight={300}
+          fontStyle="normal"
+        />
+      )}
     </>
   )
 }
