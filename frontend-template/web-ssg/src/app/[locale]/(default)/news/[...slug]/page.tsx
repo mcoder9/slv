f1a535068485de35697e461db858c98e7ce1bef1
KishiTheMechanic <s.kishi@elsoul.nl>
2025-03-18 15:35:41 +0100

    fix md error and show json-ld when article has video

--- frontend-template/web-ssg/src/app/[locale]/(default)/news/[...slug]/page.tsx
+++ frontend-template/web-ssg/src/app/[locale]/(default)/news/[...slug]/page.tsx
@@ -14,6 +14,7 @@ import { AspectRatio } from '@/components/ui/aspect-ratio'
 import ArticleIndex from '@/components/articles/ArticleIndex'
 import ArticlePager from '@/components/articles/ArticlePager'
 import { getPagerData } from '@/lib/getPagerData'
+import VideoJsonLd from '@/components/articles/VideoJsonLd'
 
 const groupDir = 'news'
 const { generateMetadata, generateStaticParams, getArticlePaths } =
@@ -23,20 +24,17 @@ export { generateMetadata, generateStaticParams }
 export default async function NewsArticlePage({ params }: ArticlePageProps) {
   const { locale, slug } = await params
   setRequestLocale(locale)
-
   const articleData = getArticleBySlug(
     slug,
     ['title', 'category', 'thumbnail', 'date', 'content'],
     groupDir,
     locale
   )
-
   const articlesData = getArticleForIndex(
     groupDir,
     ['title', 'thumbnail', 'date'],
     locale
   )
-
   const pagerData = getPagerData({
     slug,
     groupDir,
@@ -44,12 +42,17 @@ export default async function NewsArticlePage({ params }: ArticlePageProps) {
     articlePaths: getArticlePaths()
   })
 
+  const { title, content, thumbnail, date } = articleData as {
+    title: string
+    content: string
+    thumbnail: string
+    date: string
+  }
+
   return (
     <>
-      <NewsMobileHeader
-        articleTitle={articleData.title as string}
-        articleContent={articleData.content as string}
-      />
+      <VideoJsonLd content={content} title={title} date={date} />
+      <NewsMobileHeader articleTitle={title} articleContent={content} />
       <div className="mx-auto max-w-4xl p-3 py-8 pt-24 text-center">
         <time
           dateTime={articleData.date as string}
@@ -64,11 +67,11 @@ export default async function NewsArticlePage({ params }: ArticlePageProps) {
       <div className="mx-auto max-w-5xl p-3 md:py-6">
         <AspectRatio ratio={16 / 9}>
           <Image
-            src={articleData.thumbnail as string}
+            src={thumbnail}
             unoptimized
             width={160}
             height={90}
-            alt={articleData.title as string}
+            alt={date}
             className="w-full rounded-xl"
           />
         </AspectRatio>
@@ -76,20 +79,19 @@ export default async function NewsArticlePage({ params }: ArticlePageProps) {
       <div className="mx-auto max-w-4xl p-3">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
           <div className="p-4 md:col-span-2">
-            <ArticleContents content={articleData.content as string} />
+            <ArticleContents content={content} />
             <div className="my-16">
               <ArticlePager pagerData={pagerData} />
             </div>
           </div>
-
           <div className="max-h-full p-4 md:col-span-1">
             <div
               className={cn(
-                'scrollbar-thumb-rounded-full scrollbar-track-rounded-full overflow-auto scrollbar-thin scrollbar-track-white scrollbar-thumb-zinc-300 dark:scrollbar-track-zinc-950 dark:scrollbar-thumb-zinc-600',
+                'scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thin scrollbar-track-white scrollbar-thumb-zinc-300 dark:scrollbar-track-zinc-950 dark:scrollbar-thumb-zinc-600 overflow-auto',
                 'hidden max-h-[calc(100vh-10rem)] md:sticky md:top-32 md:block'
               )}
             >
-              <ScrollSyncToc rawMarkdownBody={articleData.content as string} />
+              <ScrollSyncToc rawMarkdownBody={content} />
             </div>
           </div>
         </div>
