2f7ee22280430f5419657e47fe701e3fd6fb2e42
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2025-04-23 09:48:27 +0200

    update - v start/stop/update:script

--- cmn/constants/version.ts
+++ cmn/constants/version.ts
@@ -5,11 +5,11 @@
 export const VERSION = '0.9.18'
 
 // Component versions
-export const VERSION_SOLANA_TESTNET = '2.2.7'
+export const VERSION_SOLANA_TESTNET = '2.2.11'
 export const VERSION_SOLANA_MAINNET = '2.1.21'
-export const VERSION_JITO_TESTNET = '2.2.7'
+export const VERSION_JITO_TESTNET = '2.2.11'
 export const VERSION_JITO_MAINNET = '2.1.21'
-export const VERSION_FIREDANCER_TESTNET = '0.408.20113'
+export const VERSION_FIREDANCER_TESTNET = '0.411.20121'
 export const VERSION_FIREDANCER_MAINNET = '0.411.20121'
 export const VERSION_GEYSER_YELLOWSTONE = 'v6.0.0+solana.2.2.4'
 export const VERSION_JUPITER_API = 'v6.0.43'
