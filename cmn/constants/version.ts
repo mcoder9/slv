778d24d675ea09ae11ceb5f9bf3cac72563a6a40
mcoder9 <49817513+mcoder9@users.noreply.github.com>
2025-04-26 00:11:52 +0400

    Update version.ts

--- cmn/constants/version.ts
+++ cmn/constants/version.ts
@@ -6,10 +6,10 @@ export const VERSION = '0.9.22'
 
 // Component versions
 export const VERSION_SOLANA_TESTNET = '2.2.12'
-export const VERSION_SOLANA_MAINNET = '2.1.21'
+export const VERSION_SOLANA_MAINNET = '2.1.16'
 export const VERSION_JITO_TESTNET = '2.2.12'
-export const VERSION_JITO_MAINNET = '2.1.21'
+export const VERSION_JITO_MAINNET = '2.1.16'
 export const VERSION_FIREDANCER_TESTNET = '0.411.20121'
 export const VERSION_FIREDANCER_MAINNET = '0.411.20121'
-export const VERSION_GEYSER_YELLOWSTONE = 'v6.0.0+solana.2.2.12'
+export const VERSION_GEYSER_YELLOWSTONE = 'v5.0.1+solana.2.1.16'
 export const VERSION_JUPITER_API = 'v6.0.43'
