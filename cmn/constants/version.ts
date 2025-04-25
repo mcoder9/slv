e101f7e4560fd2d1dfc2be29f90d9eeb9488f43a
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2025-04-25 14:06:18 +0200

    Release v0.9.22

--- cmn/constants/version.ts
+++ cmn/constants/version.ts
@@ -2,7 +2,7 @@
 // e.g.: VERSION + SOLANA CLI NAME + NETWORK = '0.0.1' only numbers and dots
 
 // SLV version
-export const VERSION = '0.9.21'
+export const VERSION = '0.9.22'
 
 // Component versions
 export const VERSION_SOLANA_TESTNET = '2.2.12'
